063ef4677f32c132256cdfba9d07914d1087000c
Christian Höltje <docwhat@gerf.org>
2018-12-10 14:08:39 -0500

    chef-workstation did not change ~/.chefdk dir name

--- etc/rbenv.d/rehash/rbenv-chefdk.bash
+++ etc/rbenv.d/rehash/rbenv-chefdk.bash
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-
 if [ -d "${RBENV_ROOT}/versions/chef-workstation" ]; then
   chef_workstation_ruby_exe="/opt/chef-workstation/embedded/bin/ruby"
   chef_workstation_ruby_lib_version=$("$chef_workstation_ruby_exe" -e 'puts RbConfig::CONFIG["ruby_version"]')
 
-  make_shims "${HOME}/.chef-workstation/gem/ruby/${chef_workstation_ruby_lib_version}/bin/"*
+  make_shims "${HOME}/.chefdk/gem/ruby/${chef_workstation_ruby_lib_version}/bin/"*
   make_shims /opt/chef-workstation/bin/*
   make_shims "/opt/chef-workstation/embedded/lib/ruby/gems/${chef_workstation_ruby_lib_version}/bin/"*
 
--- etc/rbenv.d/which/rbenv-chefdk.bash
+++ etc/rbenv.d/which/rbenv-chefdk.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ "$RBENV_VERSION" = chef-workstation ]]; then
+if [[ $RBENV_VERSION == chef-workstation ]]; then
   if [ ! -x "$RBENV_COMMAND_PATH" ]; then
     chef_workstation_ruby_exe="/opt/chef-workstation/embedded/bin/ruby"
     chef_workstation_ruby_lib_version=$("$chef_workstation_ruby_exe" -e 'puts RbConfig::CONFIG["ruby_version"]')
-    if [ -x "${HOME}/.chef-workstation/gem/ruby/${chef_workstation_ruby_lib_version}/bin/${RBENV_COMMAND}" ]; then
-      RBENV_COMMAND_PATH="${HOME}/.chef-workstation/gem/ruby/${chef_workstation_ruby_lib_version}/bin/${RBENV_COMMAND}"
+    if [ -x "${HOME}/.chefdk/gem/ruby/${chef_workstation_ruby_lib_version}/bin/${RBENV_COMMAND}" ]; then
+      RBENV_COMMAND_PATH="${HOME}/.chefdk/gem/ruby/${chef_workstation_ruby_lib_version}/bin/${RBENV_COMMAND}"
     elif [ -x "/opt/chef-workstation/bin/${RBENV_COMMAND}" ]; then
       RBENV_COMMAND_PATH="/opt/chef-workstation/bin/${RBENV_COMMAND}"
     elif [ -x "/opt/chef-workstation/embedded/bin/${RBENV_COMMAND}" ]; then
