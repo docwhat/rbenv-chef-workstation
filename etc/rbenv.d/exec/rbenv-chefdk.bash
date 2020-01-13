4c5ad264116d6ae86862fe9cf80fa9b3b0a4ceb7
Christian Höltje <docwhat@gerf.org>
2020-01-13 12:39:39 -0500

    fix(exec): only use posix shell init for PATHs

--- etc/rbenv.d/exec/rbenv-chefdk.bash
+++ etc/rbenv.d/exec/rbenv-chefdk.bash
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "$RBENV_VERSION" = chef-workstation ]]; then
-  if [ -x "/opt/chef-workstation/bin/chef-cli" ]; then # check for chef-cli, which was renamed from chef binary starting workstation v0.10.41 
-    eval "$(/opt/chef-workstation/bin/chef-cli shell-init $RBENV_SHELL)"
+# Run shell-init for POSIX shell so that we have the correct PATHs, etc.
+# We don't care about the completion so a POSIX shell init is  enough.
+if [[ $RBENV_VERSION == chef-workstation ]]; then
+  # check for chef-cli, which was renamed from chef binary starting workstation v0.10.41
+  if [ -x "/opt/chef-workstation/bin/chef-cli" ]; then
+    eval "$(/opt/chef-workstation/bin/chef-cli shell-init sh)"
   else
-    eval "$(/opt/chef-workstation/bin/chef shell-init $RBENV_SHELL)"
+    eval "$(/opt/chef-workstation/bin/chef shell-init sh)"
   fi
 fi
